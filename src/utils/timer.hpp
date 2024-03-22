47dcb2d272ecd3257d71f6e06305b1c06ec87e9a
vious <viousxie@outlook.com>
2024-03-22 12:39:35 +0800

    Timer

--- src/utils/timer.hpp
+++ src/utils/timer.hpp
@@ -1,5 +1,5 @@
-/* reference to https://github.com/eglimi/cpptime
-and https://github.com/li199603/cpp_toys/timer/Timer.hpp
+/* reference: https://github.com/eglimi/cpptime
+and https://github.com/li199603/cpp_toys/blob/master/timer/Timer.hpp
  */
 #pragma once
 #include <mutex>
@@ -8,28 +8,48 @@ and https://github.com/li199603/cpp_toys/timer/Timer.hpp
 #include <chrono>
 #include <functional>
 #include <vector>
+#include <map>
+#include <stack>
 
 
 namespace NaiveTimer 
 { // namespace NaiveTimer
 
+using TimerId = std:;size_t;
 using FuncType = std::function<void()>;
 using Duration = std::chrono::microseconds;
 using StdyClock = std::chrono::steady_clock;
 using Timestamp = std::chrono::time_point<StdyClock>;
 
 struct Task {
-    std::size_t task_id;
+    TimerId task_id;
+    Timestamp start_time;
     Duration period;
-
+    
     FuncType func;
+    bool isValid;
+    bool repeatable;
+
+    Task(): task_id(0), start_time(Duration::zero()), period(Duration::zero()), func(nullptr), repeatable(false), isValid(false) {}
+
+    Task(TimerId t_id, Timestamp start, Duration period, FuncType&& func, bool repeatable)
+        : task_id(t_id), start_time(start), period(period), func(func), repeatable(repeatable), isValid(true)
+    {}
+
+    Task(Task&& task) = default;
+    Task& operator=(Task&& task) = default;
+    Task(const Task& task) = delete;
+    Task& operator=(const Task& task) = delete;
 
 };
 
 
 class Timer {
 public:
-
+    Timer();
+    ~Timer();
+    TimerId add();
+    bool remove(TimerId);
 
 private:
     std::thread worker_thread;
@@ -37,6 +57,13 @@ private:
     std::condition_variable tm_condVar;
     std::atomic<bool> running_status;
 
+    TimerId tm_curId;
+
+    std::vector<Task> tasks;
+    // use multimap
+    
+
+    void run();
     
 };
 
