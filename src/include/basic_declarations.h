4bb367301ccfc18f260d8b87f0fb9f65f3f664da
Vious <viousxie@outlook.com>
2024-08-28 19:44:46 +0800

    add define file

---
+++ src/include/basic_declarations.h
@@ -0,0 +1,46 @@
+/* include defitions of some needed variables */
+
+#pragma once
+#include <string>
+#include <memory>
+#include <utility>
+#include <cstring>
+#include <vector>
+
+// #ifndef EXPORT_FLAG
+// #define EXPORT_FLAG __attribute__((__visibility__("default")))
+// #endif
+
+// define Exchange ID
+#define EXCHANGE_SHFE    "SHFE"  // Shanghai Futures Exchange
+#define EXCHANGE_CFFEX   "CFFEX" // China Financial Futures Exchange
+#define EXCHANGE_GFEX    "GFEX"  // Guangzhou Futures Exchange
+
+#define EXCHANGE_INE     "INE"   // Shanghai International Energy Exchange
+
+#define EXCHANGE_DCE     "DCE"   // Dalian Commodity Exchange
+#define EXCHANGE_CZCE    "CZCE"  // Zhengzhou Commodity Exchange
+
+namespace naiveTrader
+{
+
+struct ExCode;
+
+struct DailyMarketData;
+
+struct TickData;
+
+struct PositionData;
+
+
+enum class OrderStatus;
+
+enum class PositionOffsetType;
+
+enum class TradeDirection;
+
+enum class EventType;
+
+enum class ErrorType;
+
+} // namespace naiveTrader
