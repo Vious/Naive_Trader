75cc554d77b60197b490ad9cd9cae9e9663edc2f
Vious <viousxie@outlook.com>
2024-09-30 17:44:46 +0800

    meta get functions for params

--- src/include/params.hpp
+++ src/include/params.hpp
@@ -7,7 +7,7 @@
 
 namespace naiveTrader
 {
-
+ 
 using Str2StrMap = std::map<std::string, std::string>;
 
 class Params {
@@ -36,7 +36,137 @@ public:
         }
     }
 
-    
+    template<typename T>
+    T get(const char key[]) const {
+        return get<T>(std::string(key));
+    }
+
+private:
+    /* char* Type */
+    template <typename T>
+    typename std::enable_if<std::is_same<T, const char*>::value, T>::type get(const std::string &key) const {
+        auto iter = mParamsMap.find(key);
+        if (iter == mParamsMap.end()) {
+            throw std::invalid_argument("key not find: " + key);
+        }
+        return iter->second.c_str();
+    }
+
+    /* string type */
+    template <typename T>
+    typename std::enable_if<std::is_same<T, std::string>::value, T>::type get(const std::string &key) const {
+        auto iter = mParamsMap.find(key);
+        if (iter == mParamsMap.end()) {
+            throw std::invalid_argument("key not find: " + key);
+        }
+        return iter->second;
+    }
+
+    /* create excode type */
+    template <typename T>
+    typename std::enable_if<std::is_same<T, ExCode>::value, T>::type get(const std::string &key) const {
+        auto iter = mParamsMap.find(key);
+        if (iter == mParamsMap.end()) {
+            throw std::invalid_argument("key not find: " + key);
+        }
+        return ExCode(iter->second.c_str());
+    }
+
+    /* int8_t/uint8_t */
+    template <typename T>
+    typename std::enable_if<std::is_same<T, int8_t>::value, T>::type get(const std::string &key) const {
+        auto iter = mParamsMap.find(key);
+        if (iter == mParamsMap.end()) {
+            throw std::invalid_argument("key not find: " + key);
+        }
+        return static_cast<int8_t>(std::atoi(iter->second.c_str() ));
+    }
+
+    template <typename T>
+    typename std::enable_if<std::is_same<T, uint8_t>::value, T>::type get(const std::string &key) const {
+        auto iter = mParamsMap.find(key);
+        if (iter == mParamsMap.end()) {
+            throw std::invalid_argument("key not find: " + key);
+        }
+        return static_cast<uint8_t>(std::atoi(iter->second.c_str() ));
+    }
+
+    /* int16_t/uint16_t */
+    template <typename T>
+    typename std::enable_if<std::is_same<T, int16_t>::value, T>::type get(const std::string &key) const {
+        auto iter = mParamsMap.find(key);
+        if (iter == mParamsMap.end()) {
+            throw std::invalid_argument("key not find: " + key);
+        }
+        return static_cast<int16_t>(std::atoi(iter->second.c_str() ));
+    }
+
+    template <typename T>
+    typename std::enable_if<std::is_same<T, uint16_t>::value, T>::type get(const std::string &key) const {
+        auto iter = mParamsMap.find(key);
+        if (iter == mParamsMap.end()) {
+            throw std::invalid_argument("key not find: " + key);
+        }
+        return static_cast<uint16_t>(std::atoi(iter->second.c_str() ));
+    }
+
+    /* int32_t/uint32_t */
+    template <typename T>
+    typename std::enable_if<std::is_same<T, int32_t>::value, T>::type get(const std::string &key) const {
+        auto iter = mParamsMap.find(key);
+        if (iter == mParamsMap.end()) {
+            throw std::invalid_argument("key not find: " + key);
+        }
+        return std::atoi(iter->second.c_str());
+    }
+
+    template <typename T>
+    typename std::enable_if<std::is_same<T, uint32_t>::value, T>::type get(const std::string &key) const {
+        auto iter = mParamsMap.find(key);
+        if (iter == mParamsMap.end()) {
+            throw std::invalid_argument("key not find: " + key);
+        }
+        return static_cast<uint32_t>(std::atoi(iter->second.c_str() ));
+    }
+
+    /* int64_t/uint64_t */
+    template <typename T>
+    typename std::enable_if<std::is_same<T, int64_t>::value, T>::type get(const std::string &key) const {
+        auto iter = mParamsMap.find(key);
+        if (iter == mParamsMap.end()) {
+            throw std::invalid_argument("key not find: " + key);
+        }
+        return std::atoll(iter->second.c_str());
+    }
+
+    template <typename T>
+    typename std::enable_if<std::is_same<T, uint64_t>::value, T>::type get(const std::string &key) const {
+        auto iter = mParamsMap.find(key);
+        if (iter == mParamsMap.end()) {
+            throw std::invalid_argument("key not find: " + key);
+        }
+        return static_cast<uint64_t>(std::atoll(iter->second.c_str() ));
+    }
+
+    /* double_t */
+    template <typename T>
+    typename std::enable_if<std::is_same<T, double_t>::value, T>::type get(const std::string &key) const {
+        auto iter = mParamsMap.find(key);
+        if (iter == mParamsMap.end()) {
+            throw std::invalid_argument("key not find: " + key);
+        }
+        return std::atof(iter->second.c_str());
+    }
+
+    /* bool */
+    template <typename T>
+    typename std::enable_if<std::is_same<T, bool>::value, T>::type get(const std::string &key) const {
+        auto iter = mParamsMap.find(key);
+        if (iter == mParamsMap.end()) {
+            throw std::invalid_argument("key not find: " + key);
+        }
+        return iter->second == "True" || iter->second == "true" || iter->second == "TRUE" || std::atoll(iter->second.c_str()) > 0;
+    }
 
 
 };
