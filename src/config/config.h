f30054e11a89562c15625efa0e48a493cc26e000
Vious <viousxie@outlook.com>
2024-10-10 18:22:06 +0800

    simple config pattern

---
+++ src/config/config.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+
+namespace naiveTrader
+{
+
+    namespace userconfig 
+    {
+        static std::string TraderFlow = "../../data/";
+        static std::string TradeFrontAddr = "tcp://180.168.146.187:10130";
+        static std::string MarketFrontAddr = "tcp://180.168.146.187:10131";
+        static std::string BrokerID = "";
+        static std::string UserID = "";
+        static std::string ProductInfo = "";
+        static std::string AuthCode = "0000000000000000";
+        static std::string AppID = "simnow_client_test";
+        static std::string Password = "";
+        // static std::string CFFEX = "CFFEX";
+        // static std::string CZCE = "CZCE";
+        // static std::string DCE = "DCE";
+        // static std::string INE = "INE";
+        static std::string SHFE = "SHFE";
+
+    }
+
+
+} // namespace naiveTrader
